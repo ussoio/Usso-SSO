f873972d9cd27f600defad96ec51e22ee7d4d6cb
Mahdi Kiani <mahdikiany@gmail.com>
2024-01-27 09:01:34 +0000

    add content-type in cors headers

--- app/middlewares/cors.py
+++ app/middlewares/cors.py
@@ -9,33 +9,30 @@ origins = {"http://localhost", "http://localhost:3000"}
 
 class DynamicCORSMiddleware(BaseHTTPMiddleware):
     async def get_allowed_origins(self, origins=origins, **kwargs):
-        websites = Website.list()
+        websites = await Website.list().skip(0).limit(10).to_list()
         if kwargs.get("origin"):
             origins.add(kwargs["origin"])
             return origins
-        return origins | {
-            f"https://{website.origin}" async for website in websites.skip(0).limit(10)
-        }
+        return origins | {f"https://{website.origin}" for website in websites}
 
     async def dispatch(self, request: fastapi.Request, call_next):
         response: fastapi.Response = await call_next(request)
 
         request.headers.get("access-control-request-headers")
-        headers = {}
-        origin = f"https://{request.url.hostname}"
-        origins = await self.get_allowed_origins(origin=origin)
+
+        if request.headers.get("origin"):
+            origin = request.headers.get("origin")
+        allowed_origins = await self.get_allowed_origins(origin=origin)
 
         # Dynamically check if the origin is allowed
-        if origin in origins:
+        headers = {}
+        if origin in allowed_origins:
             headers["Access-Control-Allow-Origin"] = origin
             headers["Access-Control-Allow-Credentials"] = "true"
-            # Add other CORS headers as needed
             headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-            headers["Access-Control-Allow-Headers"] = "*"
+            headers["Access-Control-Allow-Headers"] = "Content-Type, *"
 
         if request.method == "OPTIONS":
-            # if requested_headers is not None:
-            #     headers["Access-Control-Allow-Headers"] = requested_headers
             return PlainTextResponse("OK", status_code=200, headers=headers)
 
         response.headers.update(headers)
