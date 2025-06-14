85d6aa30276cc997c8858a48c2f127b9595992bb
Mahdi Kiani <mahdikiany@gmail.com>
2025-06-14 15:41:16 +0000

    add uv

---
+++ app/web/_next/static/chunks/app/loading-06d04ae62e732c1d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[209],{3004:(e,s,_)=>{Promise.resolve().then(_.bind(_,8086))}},e=>{var s=s=>e(e.s=s);e.O(0,[352,86,441,684,358],()=>s(3004)),_N_E=e.O()}]);
